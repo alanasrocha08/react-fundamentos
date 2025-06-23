c6b97d19f2dd617d44f7b63db370480736f39ea5
alanasrocha08 <alanasr02@gmail.com>
2025-06-23 09:36:42 -0300

    Comentário explicando para que serve cada coisa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Ajuda a destectar uso de recursos obsoletos (APIS, funções, padrões etc)
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
+// Usado para cria a 'raiz' da aplicação React a partir de um elemento HTML
+import { createRoot } from "react-dom/client";
+
+// Importa o componente principal da aplicação.
+// O componente App é o ponto de entrada da aplicação React
+import App from "./App.tsx";
+
+// Seleciona o elemto HTML root, cria raíz da aplicação e renderiza o componente App.
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
