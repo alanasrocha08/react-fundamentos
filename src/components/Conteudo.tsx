05197668cd3513b58d46f322e961eda87eb9f5e3
alanasrocha08 <alanasr02@gmail.com>
2025-06-24 09:14:35 -0300

    Cria componente para artigo

--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,32 +7,11 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Esre é um exemplo de aplicação React.</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius totam
-            dolores maiores ducimus mollitia eveniet dolore, quia temporibus
-            sint sequi!
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 2 de exemplo</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius totam
-            dolores maiores ducimus mollitia eveniet dolore, quia temporibus
-            sint sequi!
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 3 de exemplo</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius totam
-            dolores maiores ducimus mollitia eveniet dolore, quia temporibus
-            sint sequi!
-          </p>
-        </article>
+        <Artigo />
       </section>
     </main>
   );
---
+++ src/components/artigo.tsx
@@ -0,0 +1,12 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius totam
+        dolores maiores ducimus mollitia eveniet dolore, quia temporibus sint
+        sequi!
+      </p>
+    </article>
+  );
+}
