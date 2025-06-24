ca2c809f0281cd4e5c4eef4429c6153dec966912
alanasrocha08 <alanasr02@gmail.com>
2025-06-24 08:59:14 -0300

    Corrige

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ export default function App() {
     <>
       <Cabecalho />
 
-      <Menu />
-
       <Conteudo />
 
       <Rodape />
--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,5 +1,6 @@
 /* Tudo o que dor usado dentro de componentes (imagens, fontes etc), deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
 import logo from "../assets/logo.png";
+import Menu from "./Menu";
 
 export default function Cabecalho() {
   return (
@@ -9,6 +10,7 @@ export default function Cabecalho() {
         Olá react!😊
       </h1>
       <hr />
+      <Menu />
     </header>
   );
 }
