3d0f119277a023dba350fec5ed46431077bb4c19
alanasrocha08 <alanasr02@gmail.com>
2025-06-24 08:59:50 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
-import Menu from "./components/Menu";
 import Rodape from "./components/Rodape";
 
 export default function App() {
