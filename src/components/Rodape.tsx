3700a8e1e769301c68c82dcb31a8574e799c2321
alanasrocha08 <alanasr02@gmail.com>
2025-06-24 11:23:08 -0300

    .

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -13,7 +13,8 @@ export default function Rodape() {
       <footer style={estilosRodape}>
         <h2>Pequeno site criado com React</h2>
         <p style={{ color: "red", fontWeight: "bold" }}>
-          Desenvolvido por <b>Alana</b> &copy; 2025
+          Desenvolvido por
+          <b>Alana</b> &copy; 2025
         </p>
       </footer>
       <p className="outro-texto">Qualquer coisa...</p>
