dab3b349a7d3e73ce2b0199aa6426e22104593c4
alanasrocha08 <alanasr02@gmail.com>
2025-06-25 10:50:33 -0300

    .

--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
+// Simulando uma mudança de tema
+let tema: boolean = false;
+
 const StyledDicaDoDia = styled.section`
-  background-color: lightgreen;
+  // Avaliando se o tema simulado é true (aplica verde) ou false (aplica azul)
+  background-color: ${tema ? "lightgreen" : "lightblue"};
   border-left: 5px solid darkgreen;
   padding: 1rem;
   margin-top: 2rem;
