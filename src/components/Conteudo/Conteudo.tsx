1abd1b5081c8c23bb75c0506ef53c65dfbac97b9
alanasrocha08 <alanasr02@gmail.com>
2025-06-25 09:15:04 -0300

    Adiciona css no conteudo

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -1,9 +1,11 @@
 import Artigo from "../Artigo/artigo";
+import estilos from "../Conteudo/Conteudo.module.css";
 
 export default function Conteudo() {
   return (
-    <main>
-      <section>
+    <main className={estilos.conteudo}>
+      {/* Para combinar classes, use temple string (aspas ``) */}
+      <section className={`${estilos.arredondada} ${estilos.sombra}`}>
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Esre é um exemplo de aplicação React.</p>
 
