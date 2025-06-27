b58cd4351c0907987c1c9263c26567cdbf72746c
alanasrocha08 <alanasr02@gmail.com>
2025-06-27 09:40:09 -0300

    Update Cabecalho.tsx

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -9,9 +9,13 @@ export default function Cabecalho() {
   No caso do useState, usando desestruturação, criamos uma constante que representa o valor do state (titulo) e uma função responsável por atualizar o state (setTitulo)*/
   const [titulo, setTitulo] = useState("Olá React!");
 
+  const alternarTitulo = () => {
+    setTitulo("Experiemntando States!");
+  };
+
   return (
     <header className={estilos.topoSite}>
-      <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
+      <h1 onClick={alternarTitulo} className={estilos.titulo}>
         <img className={estilos.logo} src={logo} alt="" /> {titulo} 😊
       </h1>
       <hr />
