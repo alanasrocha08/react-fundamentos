f2f9f52e7f2620511549627c6e3d64ec86a4c676
alanasrocha08 <alanasr02@gmail.com>
2025-06-27 11:43:22 -0300

    Update Conteudo.tsx

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -5,8 +5,11 @@ import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import Saudacao from "./saudacao";
 import cursos from "../../Data/cursos";
 import ListaCursos from "../ListaCursos/ListaCursos";
+import { useState } from "react";
 
 export default function Conteudo() {
+  const [categoriaAtiva, setCategoriaAtiva] = useState<null | string>(null);
+
   const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
 
   return (
@@ -20,12 +23,28 @@ export default function Conteudo() {
 
         {categorias.map((categoria) => {
           return (
-            <button className="rounded py-1 px-2 mr-1 bg-blue-500 hover:bg-red-500">
+            <button
+              onClick={() => setCategoriaAtiva(categoria)}
+              className="rounded py-1 px-2 mr-1 bg-blue-500 hover:bg-red-500"
+            >
               {categoria}
             </button>
           );
         })}
 
+        {categoriaAtiva && (
+          <>
+            <button
+              onClick={() => setCategoriaAtiva(null)}
+              className="p-2 border rounded bg-red-400"
+            >
+              Limpar{" "}
+            </button>
+            <p>
+              Categoria seleceionada: <b>{categoriaAtiva}</b>
+            </p>
+          </>
+        )}
         <ListaCursos />
       </section>
 
