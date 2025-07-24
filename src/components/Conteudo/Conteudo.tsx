36f7f7cae5aa9e23b8952fb9d2fc2c7126d277bb
alanasrocha08 <alanasr02@gmail.com>
2025-07-24 09:01:31 -0300

    Update Conteudo.tsx

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -45,7 +45,7 @@ export default function Conteudo() {
             </p>
           </>
         )}
-        <ListaCursos />
+        <ListaCursos categoria={categoriaAtiva} />
       </section>
 
       <DicaDoDia />
