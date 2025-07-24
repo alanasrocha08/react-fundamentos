8796c3b9002e3121647baf7514723a4c1a53de46
alanasrocha08 <alanasr02@gmail.com>
2025-07-24 10:05:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
+import Home from "./components/pages/Home";
+import Sobre from "./components/pages/Sobre";
+import Contato from "./components/pages/Contato";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
 
-      <Conteudo />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
 
       <Rodape />
     </>
